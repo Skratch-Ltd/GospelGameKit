8b787f7f59edc7378ef9d9f390de4e6ddbebcea2
Aaron Matenga <matenga.aaron@gmail.com>
2015-09-15 23:08:23 +1200

    undo test

--- js/app.js
+++ js/app.js
@@ -127,21 +127,6 @@
       })
     }
 
-    if (game.title == 'Heads Up') {
-      function onSuccess(acceleration) {
-        alert('Acceleration X: ' + acceleration.x + '\n' +
-              'Acceleration Y: ' + acceleration.y + '\n' +
-              'Acceleration Z: ' + acceleration.z + '\n' +
-              'Timestamp: '      + acceleration.timestamp + '\n');
-      };
-
-      function onError() {
-        alert('onError!');
-      };
-
-      navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
-    }
-
     $scope.nextWord = function() {
       index++;
       if (index == wordList.length)
